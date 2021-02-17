2e5b045f8ef3c7ee35205f990fa76b9c8d46ec39
Mohammed <atefhamosa@gmail.com>
2021-02-18 00:00:16 +0200

    Make through tble

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
     belongs_to :user, class_name: "User", foreign_key: "creator_id"
+    has_many :attendees, class_name: "User", through: :attendance, foreign_key: "attendee_id"
+    has_many :attendances
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_163126) do
+ActiveRecord::Schema.define(version: 2021_02_17_211711) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.integer "event_id"
+    t.integer "attendee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
