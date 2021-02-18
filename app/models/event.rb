d25d7e8f1d7568d3b65fa26b093313d34ab21c73
Mohammed <atefhamosa@gmail.com>
2021-02-19 00:17:30 +0200

    Change

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ApplicationRecord
     belongs_to :user, class_name: "User", foreign_key: "creator_id"
     has_many :attendees, class_name: "User", through: :attendance, foreign_key: "attendee_id"
     has_many :attendances
-    scope :future, ->{ where( "date > #{Time.now}" ) }
-    scope :past, ->{ where( "date < #{Time.now}" ) }
+    scope :future, ->{ where("date > #{Time.now.to_s[0..9]}") }
+    # scope :past, ->{ where( "date < #{Time.now.to_datetime}" ) }
 end
