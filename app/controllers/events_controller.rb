83646e40b150db77abb6a8d99455f22bb2bba2a6
afcastaneda223 <af.castaneda223@gmail.com>
2021-02-18 14:10:35 -0300

    added bootstrap to new and show added control to attendee

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,7 @@ class EventsController < ApplicationController
 
     def create
         @user = User.find(session[:current_user]["id"])
-        @event = @user.events.build(event_params)
+        @event = @user.created_events.build(event_params)
 
         if @event.save
             redirect_to events_path
@@ -18,9 +18,20 @@ class EventsController < ApplicationController
         @attendances = @event.attendances
     end
 
+    def attend
+        @user = User.find(session[:current_user]["id"])
+        @attend = @user.attendances.build(event_id: params[:id])
+
+        if @attend.save
+            redirect_to event_path(params[:id])
+        end
+        
+    end
+
     private
 
     def event_params
         params.require(:event).permit(:description, :name, :date, :place)
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "users#new"
   resources :users, only: [:new, :create, :show]
   resources :events, only: [:index, :show, :create]
+  get "/events/attend/", to: "events#attend"
 end
