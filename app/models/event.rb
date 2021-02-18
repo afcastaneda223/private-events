1986a2d70f0ed3f2b8b53d1420d6a83434994a95
Mohammed <atefhamosa@gmail.com>
2021-02-19 00:34:43 +0200

    Fix dates

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ApplicationRecord
     belongs_to :user, class_name: "User", foreign_key: "creator_id"
     has_many :attendees, class_name: "User", through: :attendance, foreign_key: "attendee_id"
     has_many :attendances
-    scope :future, ->{ where('date >= ?', Date.today).order(:date => :DESC) }
+    scope :future, ->{ where('date >= ?', Date.today).order(:date => :ASC) }
     scope :past, ->{ where('date < ?', Date.today) }
 end
