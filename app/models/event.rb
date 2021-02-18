ef5c606fc88bcd0c146217cf972ee064ae7d678f
Mohammed <atefhamosa@gmail.com>
2021-02-19 00:31:39 +0200

    Make the scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ApplicationRecord
     belongs_to :user, class_name: "User", foreign_key: "creator_id"
     has_many :attendees, class_name: "User", through: :attendance, foreign_key: "attendee_id"
     has_many :attendances
-    scope :future, ->{ where("date > #{Time.now.to_s[0..9]}") }
-    # scope :past, ->{ where( "date < #{Time.now.to_datetime}" ) }
+    scope :future, ->{ where('date >= ?', Date.today).order(:date => :DESC) }
+    scope :past, ->{ where('date < ?', Date.today) }
 end
